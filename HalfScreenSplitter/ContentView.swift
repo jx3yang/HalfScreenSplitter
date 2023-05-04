76c428a500f0db4f1e30bdeeaef335516f5f39bd
jx3yang <65411020+jx3yang@users.noreply.github.com>
2023-05-04 18:54:57 -0400

    Add status bar menu using SwiftUI

---
+++ HalfScreenSplitter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HalfScreenSplitter
+//
+//  Created by Ji Xi Yang on 2023-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+//        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
